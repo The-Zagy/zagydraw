643523c2c5cfc688af4eac55880cba86b3cc2340
ahmedaliv <ahmed195131@feng.bu.edu.eg>
2023-05-13 21:10:34 +0300

    edited the fix of the rect creation

--- src/utils/canvas/generateElement.ts
+++ src/utils/canvas/generateElement.ts
@@ -48,15 +48,13 @@ const generateRectElement = (
     options: Partial<RectOptions & Options & { id: string }>,
     seed?: number
 ): ZagyCanvasRectElement => {
-    const {x,y,endX,endY} =  getCorrectPos(startPos,endPos);
+    const { x, y, endX, endY } = getCorrectPos(startPos, endPos);
     const opts = normalizeRectOptions(options);
-    const width = Math.max(Math.abs( endPos[0] - startPos[0]),1);
-    const height = Math.max(Math.abs( endPos[1] - startPos[1]),1);
     const r = generator.rectangle(
-        startPos[0],
-        startPos[1],
-        width,
-        height,
+        x,
+        y,
+        endX - x,
+        endY - y,
         {
             roughness: 2,
             ...opts,
@@ -66,10 +64,10 @@ const generateRectElement = (
     return {
         ...r,
         id: options.id !== undefined ? options.id : nanoid(),
-       x,
-       y,
-       endX,
-       endY,
+        x,
+        y,
+        endX,
+        endY,
         shape: "rectangle",
         curPos: curPos,
         opacity: opts.opacity
@@ -94,7 +92,7 @@ export const generateSelectRectElement = (generator: RoughGenerator,
                 stroke:"transparent",
                 roughness: 0
             },
-    );
+        );
         return {
             ...rect,
            x,
@@ -107,6 +105,7 @@ export const generateSelectRectElement = (generator: RoughGenerator,
             id: nanoid(),
         }
 }
+
 
 const generateLineElement = (
     generator: RoughGenerator,
