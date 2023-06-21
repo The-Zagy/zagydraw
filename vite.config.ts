b0eef7583920f3412385179ed7ad237a94ad5afb
sandstone991 <ahmed195138@feng.bu.edu.eg>
2023-06-21 07:23:15 +0300

    fixed a typescript error in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vitejs.dev/config 
+// https://vitejs.dev/config
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  assetsInclude: 'src/assets',
-  test: {
-    globals: true,
-    environment: 'happy-dom',
-    setupFiles: '.vitest/setup',
-    include: ['**/test.{ts,tsx}']
-  }
-})
+    plugins: [react(), tsconfigPaths()],
+    assetsInclude: "src/assets",
+    test: {
+        globals: true,
+        environment: "happy-dom",
+        setupFiles: ".vitest/setup",
+        include: ["**/test.{ts,tsx}"],
+    },
+});
