8c40f6d0943291990cb1e4c2119467e2e0105e3c
sandstone991 <ahmed195138@feng.bu.edu.eg>
2023-07-04 22:43:40 +0300

    fix useIsMobile

--- src/hooks/useIsMobile.ts
+++ src/hooks/useIsMobile.ts
@@ -5,11 +5,15 @@ const { setIsMobile } = useStore.getState();
 const useIsMobile = () => {
     const isMobileCallback = useCallback(() => {
         if (typeof window === "undefined") return false;
-        if (window.innerWidth > 768) return false;
+        if (window.innerWidth > 768) {
+            setIsMobile(false);
+            return false;
+        }
         setIsMobile(true);
     }, []);
     useGlobalEvent("resize", isMobileCallback);
     useGlobalEvent("orientationchange", isMobileCallback);
     useGlobalEvent("load", isMobileCallback);
+    useGlobalEvent("DOMContentLoaded", isMobileCallback);
 };
 export { useIsMobile };
